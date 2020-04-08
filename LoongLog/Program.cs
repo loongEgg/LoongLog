61b3f7574487dd88976014f7a7282e47cd074ed1
loongEgg <loongEgg@users.noreply.github.com>
2020-04-08 21:06:21 +0800

    01.Colorfull Console And Time Format 01.控制台的彩色输出和时间格式化

--- LoongLog/Program.cs
+++ LoongLog/Program.cs
@@ -1,14 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LoongLog
 {
     class Program
     {
+         
         static void Main(string[] args) {
+
+            ColorfullConsoleAndTimeFormat();
+             
+            Console.ReadKey();// 暂停的一种方法
         }
+
+        // TODO: 01-A 彩色的控制台输出           
+        // TODO: 01-B 时间的格式化输出
+
+        /// <summary>
+        /// 01. 控制台的彩色输出和时间格式化
+        /// </summary>
+        static void ColorfullConsoleAndTimeFormat() {
+            // 获取当前颜色（默认颜色）
+            var oldColor = Console.ForegroundColor;
+
+            // 输出一次蓝色
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("This is blue...");
+            
+            // 输出一次红色
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("This is red...");
+
+            // 输出一次黄色
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("This is yellow...");
+            
+            // 将控制台设置为默认
+            Console.ForegroundColor = oldColor;
+            Console.WriteLine("This is default...");
+
+            // 获取当前时间
+            var time = DateTime.Now;
+            Console.WriteLine($"Normal format: {time.ToString()}");
+
+            // 时间的格式化
+            Console.WriteLine($"Format string: {time.ToString("yyyy-MM-dd hh-mm-ss")}");
+        }
+
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # LoongLog
-一个可以在控制台（彩色）、输出（彩色）、文件同时记录的Log日志。A log for c# with colorful output/console and file recorder.
+一个可以在控制台（彩色）、输出（彩色）、文件同时记录的Log日志。   
+A log for c# with colorful output/console and file recorder.   
+你可以在提交历史中看到每一步的实现.  
+You can learn every step in  history commit.
+## 01.Colorfull Console And Time Format
